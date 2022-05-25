1ca67a0e7190653b93f9297520298ba127f3b07a
Adriano <kdr@outlook.com.br>
2022-05-25 10:23:19 -0300

    add routers

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SignUp from './pages/SIgnUp'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SIgnUp';
+import Stores from './pages/Stores/Stores';
+import Transactions from './pages/Transactions/Transactions';
+import UploadScreen from './pages/UploadScreen/UploadScreen';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/upload' element={<UploadScreen />} />
+        <Route path='/stores' element={<Stores />} />
+        <Route path='/transactions' element={<Transactions />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
